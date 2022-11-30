9e28d37e89a44598bf558c99615a2dd968e937f5
Mani Raj <maniraj.021199@gmail.com>
2022-11-30 18:54:31 +0530

    ADDED: EmployeeService class Added. MODIFIED: Fetch data from api.

---
+++ Web/Learnings/EmployeeManagement/EmployeeManagement.Web/Services/EmployeeService/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Web.Services
+{
+	public class EmployeeService : IEmployeeService
+	{
+		#region Fields
+		private readonly HttpClient httpClient;
+		#endregion
+
+		#region Constructors
+		public EmployeeService(HttpClient httpClient)
+		{
+			this.httpClient = httpClient;
+		}
+		#endregion
+
+		#region Publics
+		public async Task<IEnumerable<Employee>?> GetEmployees()
+		{
+			return await httpClient.GetFromJsonAsync<Employee[]>("api/Employee");
+		}
+		#endregion
+	}
+}
